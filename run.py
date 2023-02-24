337be033185b85ccfd637d1c26089e6efac02550
yousirui1 <yousirui1@163.com>
2023-02-24 19:09:06 +0800

    2023/2/24

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
 #os.environ["CUDA_VISIBLE_DEVICES"]="-1"    
 import tensorflow as tf
+from tensorflow.keras.models import Model
 import argparse
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau,EarlyStopping
 from model.mobilenet_v3_small import MobileNetV3_Small, MoblieNetV3_model
-from utils.datasets import DataGenerator
+from utils.datasets import DataGenerator, get_files_and_labels, DataGenerator_NP
 import utils.params as yamnet_params
 
 #physical_devices = tf.config.list_physical_devices('GPU')
@@ -13,14 +14,106 @@ import utils.params as yamnet_params
 print("TF GPU: {}".format(tf.test.gpu_device_name()), " enable: {}".format(tf.test.is_gpu_available()))
 print("TF version:{}".format(tf.__version__))
 
+
+def train_np(opt):
+    train_dir = '/home/ysr/dataset/audio/train_set_patches/'
+    model_out = './saved_models/model'
+    model_last_out = './saved_models/last'
+    batch_size = 1
+    epochs = 100
+
+    files_train, files_val, labels = get_files_and_labels(train_dir, 
+                                                    typ='npy',
+                                                    train_split=0.8,
+                   # wanted_label = 'Alarm,ChainSaw,Cough,Cry,Explosion,GlassBreak,Knock,Scream,Siren,Voice' #Firecracker
+                    #               ',Buzzer,Glass,Laughter,Acoustic,Carhorn,Music') #HandSaw Applause
+                 wanted_label = 'Alarm,ChainSaw,Cough,GlassBreak,Buzzer,Explosion,Siren,Digging,Smash,Voice')            
+
+    train_generator = DataGenerator_NP(files_train,
+                                labels,
+                                batch_size=batch_size,
+                                n_classes = len(labels))
+
+    valid_generator = DataGenerator_NP(files_val,
+                                    labels,
+                                    batch_size=batch_size,
+                                    n_classes = len(labels))
+
+    # Define training callbacks
+    checkpoint = ModelCheckpoint(model_out+'.h5',
+              monitor='val_loss', 
+              verbose=1,
+              save_best_only=True, 
+              mode='auto')
+
+    reducelr = ReduceLROnPlateau(monitor='val_loss', 
+                factor=0.5, 
+                patience=3, 
+                verbose=1)
+
+    earlystop = EarlyStopping(monitor='val_accuracy', patience=5, verbose=0, mode='auto')
+
+    params = yamnet_params.Params()
+    model = MobileNetV3_Small((params.patch_frames, params.mel_bands), 527).model(params)
+    model.summary()
+
+    if opt.weights and os.path.exists(opt.weights):
+        model.load_weights(opt.weights)
+        for layer in model.layers:
+            layer.trainable = False
+        #o = tf.keras.layers.Conv2D(len(labels), (1, 1), padding='same', activation='softmax')(model.layers[-6].output)
+        #o = tf.keras.layers.Reshape((len(labels),))(o)
+        #tf.keras.layers.Dense
+        o = tf.keras.layers.Dense(units=len(labels), use_bias=True)(model.layers[-6].output)
+        o = tf.keras.layers.Activation('softmax')(o) #sigmoid
+
+        model = Model(inputs=model.input, outputs=[o])
+        model.summary()
+
+
+#yer in yamnet.layers:
+    #print(layer)
+#     layer.trainable = False
+
+#o = tf.keras.layers.Dense(units=len(labels), use_bias=True)(yamnet.layers[-3].output)
+#o = tf.keras.layers.Activation('softmax')(o) #sigmoid
+
+#if tflite_ouput == 1:
+#    model = Model(inputs=yamnet.input, outputs=o)
+#else:
+#    model = Model(inputs=yamnet.input, outputs=[o, yamnet.output])
+
+#model.summary()
+
+
+    # Compile model
+    #optimizer = tf.keras.optimizers.Adam(lr=0.001) #0.001
+    optimizer = tf.keras.optimizers.SGD() #0.001
+    #categorical_crossentropy binary_crossentropy
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])   #categorical_accuracy 
+    model_history = model.fit(train_generator,
+                            steps_per_epoch = len(train_generator),
+                            batch_size = batch_size,
+                            epochs = epochs,
+                            validation_data = valid_generator,
+                            validation_steps = len(valid_generator), #validation_generator
+                            verbose = 1,
+                            #callbacks=[earlystop,checkpoint,reducelr])
+                            callbacks=[checkpoint])
+
+    model.save(model_last_out+'.h5')
+
+
+
 def train(hyp, opt):
     train_cache_dir = '/home/ysr/dataset/audio/audioset/train.cache'
     valid_cache_dir = '/home/ysr/dataset/audio/audioset/valid.cache'
     n_classes = 527
 
     model_out = './saved_models/model'
+    model_last_out = './saved_models/last'
     batch_size = 1
-    epochs = 100
+    epochs = 5
 
     train_generator = DataGenerator(train_cache_dir, batch_size, n_classes = n_classes)
     valid_generator = DataGenerator(valid_cache_dir, batch_size, train_split = 0.3,  n_classes = n_classes)
@@ -62,13 +155,13 @@ def train(hyp, opt):
                             verbose = 1,
                             callbacks=[checkpoint,reducelr])
 #                            callbacks=[earlystop,checkpoint,reducelr])
+    model.save(model_last_out+'.h5')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='', help='initial weights path')
     opt = parser.parse_args()
     train(None, opt)
-
-
-
+    #train_np(opt)
 
--- utils/datasets.py
+++ utils/datasets.py
@@ -13,6 +13,9 @@ from tensorflow.python.keras.utils.data_utils import Sequence
 from random import shuffle
 
 audio_formats = ['wav', 'aac', 'pcm']
+BACKGROUND_NOISE_DIR_NAME = '_background_noise_'
+SILENCE_LABEL = '_Silence_'
+UNKNOWN_WORD_LABEL = '_Unknown_'
 
 # PCM
 class LoadSounds:
@@ -184,6 +187,190 @@ class LoadAudioSet():
 
     def __len__(self):
         return self.cache.size()
+
+def get_files_and_labels(train_dir, typ='wav', train_split=0.9, wanted_label = None):
+    #ignored = {"folder_one", "folder_two", "folder_three"}
+    #folders = [x for x in os.listdir(path) if x not in ignored]
+    
+    if not wanted_label:
+        classes = sorted(os.listdir(train_dir))
+    else :
+        classes = [SILENCE_LABEL, UNKNOWN_WORD_LABEL] + wanted_label.split(',') 
+    files_train = list()
+    files_val = list()
+    labels = dict()
+ 
+    for cnt, i in enumerate(classes): # loop over classes
+        tmp = os.listdir(train_dir+i)
+        shuffle(tmp)
+        for j in tmp[:round(len(tmp)*train_split)]: # loop over training samples
+            if j.split('.')[-1]==typ:
+                files_train.append(train_dir + i +'/' + j)
+        for j in tmp[round(len(tmp)*train_split):]: # loop over validation samples
+            if j.split('.')[-1]==typ:
+                files_val.append(train_dir + i +'/' + j)
+        labels[i] = cnt
+    return files_train, files_val, labels
+     
+class DataGenerator_NP(Sequence):
+    
+    'Generates YAMNet patches'
+    def __init__(self, 
+                 list_IDs, 
+                 labels, 
+                 dim = (96, 64),
+                 batch_size=1, 
+                 n_classes=3,
+                 use_background = 0,
+                 data_dir = '',
+                 shuffle=True,
+                 class_weights=None):
+        'Initialization'
+        self.classes = []
+        self.batch_size = batch_size
+        self.labels = labels
+        self.dim = dim
+        self.list_IDs = list_IDs
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.class_weights = class_weights
+        self.use_background = use_background
+        self.data_dir = data_dir
+        self.prepare_background_data()
+        self.on_epoch_end()
+        
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        if self.class_weights:
+            X, y, sample_weights = self.__data_generation(list_IDs_temp)
+            return X, y, sample_weights
+        else:    
+            X, y = self.__data_generation(list_IDs_temp)
+            return X, y
+
+    def get_data(self):
+        x = 1
+        y = 1
+        return x, y
+    
+    def prepare_background_data(self):
+        self.background_data = []
+        
+        background_dir = os.path.join(self.data_dir, BACKGROUND_NOISE_DIR_NAME)
+        #if not gfile.Exists(background_dir):
+        if os.path.exists(background_dir) == False:
+            print("no background audio")
+            self.use_background = False
+            return self.background_data
+        search_path = os.path.join(self.data_dir, BACKGROUND_NOISE_DIR_NAME, '*.wav')
+        
+        #desired_samples = 16000
+        #sample_rate = 16000
+        for wav_file in sorted(glob.glob(os.path.join(search_path))):
+            print(wav_file)
+            
+            wav_data, sr = sf.read(wav_file, dtype=np.int16)
+            assert wav_data.dtype == np.int16, 'Bad sample type: %r' % wav_data.dtype
+       
+            '''
+            if len(wav_data) < 16000:
+                wav_data = np.pad(wav_data, (0, 16000 - len(wav_data)), 'constant', constant_values = 0)
+            print(len(wav_data))
+            waveform = wav_data / 32768.0
+            if len(waveform.shape) > 1:
+                waveform = np.mean(waveform, axis=1)
+            
+            if sr != params.sample_rate:
+                waveform = resampy.resample(waveform, sr, params.sample_rate)
+            
+            waveform = np.reshape(waveform, [1, -1]).astype(np.float32)
+            spectrogram, patches = features_lib. \
+                          waveform_to_log_mel_spectrogram_patches(tf.squeeze(waveform, axis=0), 
+                                                      params)
+            
+            wav_data = ess.MonoLoader(filename = wav_file, sampleRate = sample_rate)() 
+            wav_pos = 0
+            while True:     
+                if len(wav_data) >=  desired_samples:
+                    audio = wav_data[:desired_samples]
+                    wav_pos = wav_pos + desired_samples
+                    wav_data = wav_data[wav_pos:]
+                    self.background_data.append(audio)
+                    if len(wav_data) == 0:
+                        break
+                elif len(wav_data) >= desired_samples // 2:
+                    audio = np.pad(wav_data, (0, desired_samples - len(wav_data)), 'constant', constant_values = 0)
+                    self.background_data.append(audio)
+                    break
+                else:
+                    break 
+            '''
+        if not self.background_data:
+            raise Exception('No background wav files were found in ' + search_path)
+
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization   
+
+        X = np.empty((self.batch_size, *self.dim))
+        y = np.empty((self.batch_size, self.n_classes))
+        sample_weights = np.empty((self.batch_size, ))
+        
+        y[:] = 0
+        #print(list_IDs_temp)
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            #print(ID)            
+            class_id = ID.split('/')[-2]
+            #print(class_id)
+            y[i,self.labels[class_id]] = 1
+            #print(y)          
+            sample = np.load(ID)
+            
+            # if the waveform for this sample was long enough to contain multiple patches, randomly select one of the patches
+            if sample.shape[0] > 1:
+                sample = np.squeeze(sample[np.random.choice(range(sample.shape[0]), 1)])
+            '''
+            if self.use_background and number_of_certain_probability():
+                background_index = np.random.randint(len(self.background_data))
+                background_samples = self.background_data[background_index]
+                #print(background_samples)
+                if len(background_samples) < self.model_settings['desired_samples']:
+                      raise ValueError(
+                          'Background sample is too short! Need more than %d'
+                      ' samples but only %d were found' %
+                      (self.model_settings['desired_samples'], len(background_samples)))
+                sample = sample + (background_volume * background_reshaped).flatten()
+            '''    
+            X[i,] = sample
+                
+            if self.class_weights:
+                sample_weights[i] = self.class_weights[self.labels[class_id]]
+          
+        self.classes.append(y.reshape(y.shape[1]))
+
+        if self.class_weights is not None:
+            return X, y, sample_weights
+        else:
+            return X, y
 
 class DataGenerator(Sequence):
     def __init__(self, cache_dir, batch_size = 1, train_split = 1, dim = (96, 64), n_classes = 527, shuffle=True):
